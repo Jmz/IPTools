7a898aa85fdff9c7a76592520152fd28299a987d
James Owers <jamesowers@gmail.com>
2014-11-12 14:59:31 +0000

    Added IPv4 & IPv6 validation methods.

--- src/IpTools/IpTools.php
+++ src/IpTools/IpTools.php
@@ -3,18 +3,49 @@
 class IpTools implements IpValidator
 {
     /**
-     * Validate an IP address. Only supports IPv4 at the minute.
+     * Validate an IP address.
      *
      * @param $ipAddress
      * @return bool
      */
     public static function validate($ipAddress)
     {
+        if (self::validateIpV4($ipAddress) || self::validateIpV6($ipAddress)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Validate an IPv4 address.
+     *
+     * @param $ipAddress
+     * @return bool
+     */
+    public static function validateIpV4($ipAddress)
+    {
         if (filter_var($ipAddress, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
             return true;
-        } else {
-            return false;
         }
+
+        return false;
     }
+
+    /**
+     * Validate an IPv6 address.
+     *
+     * @param $ipAddress
+     * @return bool
+     */
+    public static function validateIpV6($ipAddress)
+    {
+        if (filter_var($ipAddress, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
+            return true;
+        }
+
+        return false;
+    }
+
 
 }
--- src/IpTools/IpValidator.php
+++ src/IpTools/IpValidator.php
@@ -10,4 +10,20 @@ interface IpValidator
      */
     public static function validate($ipAddress);
 
+    /**
+     * Validate an IPv4 address.
+     *
+     * @param $ipAddress
+     * @return bool
+     */
+    public static function validateIpV4($ipAddress);
+
+    /**
+     * Validate an IPv6 address.
+     *
+     * @param $ipAddress
+     * @return bool
+     */
+    public static function validateIpV6($ipAddress);
+
 }
